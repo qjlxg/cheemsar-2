5b9e4ea7be0edec0b57617d7a6602a5dff706aa7
qjlxg <qjlxg@163.com>
2025-01-10 21:12:20 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -410,13 +410,3 @@ if __name__ == '__main__':
     res = write_document()
     clash_sub = get_yaml()
     print("========== 写入完成任务结束 ==========")
-i = 0
-with open("Long_term_subscription_num", "w", encoding="utf-8") as file:
-    for code in processed_codes:
-        if i == 0:
-            config_string = "#🌐 Updated on " + final_string + ":00 | @config_kharaki"
-        else:
-            config_string = "#✅ " + str(final_string) + ":00-" + str(i)
-        config_final = code + config_string
-        file.write(config_final + "\n")
-        i += 1
